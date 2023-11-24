1adf14e089cbc0606ce56e5da3eefdea1b7772c3
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 21:10:42 +0000

    ship map test 2

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,29 @@
-// Example: Update highlight position based on player's current room
-const playerRoom = document.getElementById('highlight');
+// Example: Current room ID
+let currentRoom = "room1";
 
-function updateHighlightPosition(top, left, width, height) {
-    playerRoom.style.top = top + 'px';
-    playerRoom.style.left = left + 'px';
-    playerRoom.style.width = width + 'px';
-    playerRoom.style.height = height + 'px';
+// Add click event listeners to room highlights
+const roomHighlights = document.querySelectorAll('.roomHighlight');
+roomHighlights.forEach(highlight => {
+    highlight.addEventListener('click', () => {
+        const clickedRoom = highlight.getAttribute('data-room-id');
+        if (isAdjacent(currentRoom, clickedRoom)) {
+            updateHighlightPosition(clickedRoom);
+            currentRoom = clickedRoom;
+        } else {
+            alert('You can only move to adjacent rooms.');
+        }
+    });
+});
+
+// Function to check if two rooms are adjacent
+function isAdjacent(room1, room2) {
+    return Math.abs(room1.charAt(4) - room2.charAt(4)) + Math.abs(room1.charAt(5) - room2.charAt(5)) === 1;
 }
 
-// Call this function when the player enters a new room
-updateHighlightPosition(150, 200, 100, 100); // Example coordinates and size
+// Function to update the player's position
+function updateHighlightPosition(roomId) {
+    const playerRoom = document.getElementById('highlight');
+    const clickedRoom = document.querySelector(`[data-room-id="${roomId}"]`);
+    playerRoom.style.top = clickedRoom.style.top;
+    playerRoom.style.left = clickedRoom.style.left;
+}
