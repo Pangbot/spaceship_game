add0ce96a95d3f3dd90e85870afb698baedf8d1d
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 23:13:23 +0000

    rect test 7

--- scripts/game.js
+++ scripts/game.js
@@ -2,17 +2,21 @@
 let currentRoomId = "room1";
 
 // Add click event listeners to room highlights
-const roomHighlights = document.querySelectorAll('.roomHighlight');
-roomHighlights.forEach(highlight => {
+const roomHighlights1x1 = document.querySelectorAll('.roomHighlight1x1');
+const roomHighlights2x1 = document.querySelectorAll('.roomHighlight2x1');
+
+const allRoomHighlights = document.querySelectorAll('.roomHighlight1x1, .roomHighlight2x1');
+
+allRoomHighlights.forEach(highlight => {
     highlight.addEventListener('click', () => {
-        const clickedRoomId = highlight.getAttribute('data-room-id');
-        const clickedRoomWidth = parseInt(highlight.getAttribute('data-room-width'));
-        const clickedRoomHeight = parseInt(highlight.getAttribute('data-room-height'));
+        const clickedRoomId = highlight.getAttribute('data-room-id'); // Replace with the correct attribute
+        const clickedRoomWidth = parseInt(highlight.getAttribute('data-room-width')); // Replace with the correct attribute
+        const clickedRoomHeight = parseInt(highlight.getAttribute('data-room-height')); // Replace with the correct attribute
 
         // Check if the clicked room is different from the current room
         if (clickedRoomId !== currentRoomId) {
             // Remove highlighting from all rooms
-            roomHighlights.forEach(room => {
+            allRoomHighlights.forEach(room => {
                 room.classList.remove('highlighted');
             });
 
