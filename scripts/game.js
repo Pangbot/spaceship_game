7a7399068120bf8949297f6d1f30d554b6ff37be
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 22:27:57 +0000

    ship map test 17

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,5 @@
-// Example: Current room ID and size
-let currentRoom = { id: "room1", width: 2, height: 2 };
+// Example: Current room ID
+let currentRoomId = "room1";
 
 // Set initial player position over room1
 const initialRoomHighlight = document.getElementById('playerHighlight');
@@ -9,39 +9,35 @@ initialRoomHighlight.classList.add('highlighted', 'size2x2');
 const roomHighlights = document.querySelectorAll('.roomHighlight');
 roomHighlights.forEach(highlight => {
     highlight.addEventListener('click', () => {
-        const clickedRoom = {
-            id: highlight.getAttribute('data-room-id'),
-            width: parseInt(highlight.getAttribute('data-room-width')),
-            height: parseInt(highlight.getAttribute('data-room-height')),
-            top: parseInt(highlight.style.top.replace('px', '')),
-            left: parseInt(highlight.style.left.replace('px', ''))
-        };
+        const clickedRoomId = highlight.getAttribute('data-room-id');
+        const clickedRoomWidth = parseInt(highlight.getAttribute('data-room-width'));
+        const clickedRoomHeight = parseInt(highlight.getAttribute('data-room-height'));
 
         // Check if the clicked room is different from the current room
-        if (clickedRoom.id !== currentRoom.id) {
+        if (clickedRoomId !== currentRoomId) {
             // Remove highlighting from the current room
             const currentRoomHighlight = document.getElementById('playerHighlight');
             currentRoomHighlight.classList.remove('highlighted');
-            currentRoomHighlight.classList.remove(`size${currentRoom.width}x${currentRoom.height}`);
+            currentRoomHighlight.classList.remove(`size${clickedRoomWidth}x${clickedRoomHeight}`);
 
             // Add highlighting to the clicked room
-            const roomSizeClass = `size${clickedRoom.width}x${clickedRoom.height}`;
+            const roomSizeClass = `size${clickedRoomWidth}x${clickedRoomHeight}`;
             highlight.classList.add('highlighted', roomSizeClass);
 
             // Update player's position
-            updateHighlightPosition(clickedRoom);
+            updateHighlightPosition(clickedRoomWidth, clickedRoomHeight, highlight.style.top, highlight.style.left);
 
             // Update current room
-            currentRoom = clickedRoom;
+            currentRoomId = clickedRoomId;
         }
     });
 });
 
 // Function to update the player's position
-function updateHighlightPosition(room) {
+function updateHighlightPosition(roomWidth, roomHeight, topPosition, leftPosition) {
     const playerRoom = document.getElementById('playerHighlight');
-    playerRoom.style.top = `${room.top}px`;
-    playerRoom.style.left = `${room.left}px`;
-    playerRoom.style.width = `${room.width * 117}px`;
-    playerRoom.style.height = `${room.height * 117}px`;
+    playerRoom.style.top = topPosition;
+    playerRoom.style.left = leftPosition;
+    playerRoom.style.width = `${roomWidth * 117}px`;
+    playerRoom.style.height = `${roomHeight * 117}px`;
 }
