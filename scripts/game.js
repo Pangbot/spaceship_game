c69bdf5fd05056cf603089653fbee358fb77ff74
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 23:23:10 +0000

    rect test 10

--- scripts/game.js
+++ scripts/game.js
@@ -1,35 +1,32 @@
-console.log("Script loaded!");
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Script loaded!");
 
-// Example: Current room ID
-let currentRoomId = "room1";
+    // Example: Current room ID
+    let currentRoomId = "room1";
 
-// Add click event listeners to room highlights
-const roomHighlights1x1 = document.querySelectorAll('.roomHighlight1x1');
-const roomHighlights2x1 = document.querySelectorAll('.roomHighlight2x1');
+    // Add click event listeners to room highlights
+    const allRoomHighlights = document.querySelectorAll('.roomHighlight1x1, .roomHighlight2x1');
 
-const allRoomHighlights = document.querySelectorAll('.roomHighlight1x1, .roomHighlight2x1');
+    allRoomHighlights.forEach(highlight => {
+        highlight.addEventListener('click', () => {
+            console.log("Clicked!");
 
-allRoomHighlights.forEach(highlight => {
-    highlight.addEventListener('click', () => {
-        console.log("Clicked!"); // Check if this log is printed
+            // Check if the clicked room is different from the current room
+            const clickedRoomId = highlight.getAttribute('data-room-id');
+            if (clickedRoomId !== currentRoomId) {
+                console.log("Different room clicked!");
 
-        const clickedRoomId = highlight.getAttribute('data-room-id'); // Replace with the correct attribute
-        const clickedRoomWidth = parseInt(highlight.getAttribute('data-room-width')); // Replace with the correct attribute
-        const clickedRoomHeight = parseInt(highlight.getAttribute('data-room-height')); // Replace with the correct attribute
+                // Remove highlighting from all rooms
+                allRoomHighlights.forEach(room => {
+                    room.classList.remove('highlighted');
+                });
 
-        // Check if the clicked room is different from the current room
-        if (clickedRoomId !== currentRoomId) {
-            // Remove highlighting from all rooms
-            allRoomHighlights.forEach(room => {
-                room.classList.remove('highlighted', `size${room.getAttribute('data-room-width')}x${room.getAttribute('data-room-height')}`);
-            });
+                // Add highlighting to the clicked room
+                highlight.classList.add('highlighted');
 
-            // Add highlighting to the clicked room
-            const roomSizeClass = `size${clickedRoomWidth}x${clickedRoomHeight}`;
-            highlight.classList.add('highlighted', roomSizeClass);
-
-            // Update current room
-            currentRoomId = clickedRoomId;
-        }
+                // Update current room
+                currentRoomId = clickedRoomId;
+            }
+        });
     });
 });
