901cce73daa66f7e4c989beb72a75a856adb3b5f
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 21:03:11 +0000

    ship map test

--- scripts/game.js
+++ scripts/game.js
@@ -1,47 +1,12 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+// Example: Update highlight position based on player's current room
+const playerRoom = document.getElementById('highlight');
 
-// Circle properties
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-const radius = 20;
-
-// Movement speed
-const speed = 5;
-
-// Event listener for key presses
-window.addEventListener('keydown', handleKeyPress);
-
-function handleKeyPress(event) {
-    switch (event.key) {
-        case 'w':
-            y -= speed;
-            break;
-        case 'a':
-            x -= speed;
-            break;
-        case 's':
-            y += speed;
-            break;
-        case 'd':
-            x += speed;
-            break;
-    }
-
-    drawCircle();
+function updateHighlightPosition(top, left, width, height) {
+    playerRoom.style.top = top + 'px';
+    playerRoom.style.left = left + 'px';
+    playerRoom.style.width = width + 'px';
+    playerRoom.style.height = height + 'px';
 }
 
-function drawCircle() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw the circle
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.stroke();
-}
-
-// Initial draw
-drawCircle();
+// Call this function when the player enters a new room
+updateHighlightPosition(150, 200, 100, 100); // Example coordinates and size
