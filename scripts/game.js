720f0bc7285aac834fd6e27a1a0a01a77a4db578
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 00:47:55 +0000

    simple test

--- scripts/game.js
+++ scripts/game.js
@@ -1,23 +1,47 @@
-const canvas = document.getElementById('gameCanvas');
+const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-function init() {
-    // Initialize game state
+// Circle properties
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+const radius = 20;
+
+// Movement speed
+const speed = 5;
+
+// Event listener for key presses
+window.addEventListener('keydown', handleKeyPress);
+
+function handleKeyPress(event) {
+    switch (event.key) {
+        case 'w':
+            y -= speed;
+            break;
+        case 'a':
+            x -= speed;
+            break;
+        case 's':
+            y += speed;
+            break;
+        case 'd':
+            x += speed;
+            break;
+    }
+
+    drawCircle();
 }
 
-function update() {
-    // Update game state
+function drawCircle() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the circle
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.stroke();
 }
 
-function render() {
-    // Render game graphics
-}
-
-function gameLoop() {
-    update();
-    render();
-    requestAnimationFrame(gameLoop);
-}
-
-init();
-gameLoop();
+// Initial draw
+drawCircle();
