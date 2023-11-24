cc78e75bf04e7369eea4a523fbec83845ab30818
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-24 22:59:51 +0000

    rect test 2

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,6 @@
 // Example: Current room ID
 let currentRoomId = "room1";
 
-// Set initial player position over room1
-const initialPlayerHighlight = document.getElementById('playerHighlight');
-initialPlayerHighlight.classList.add('highlighted', 'size1x1'); // Adjust the class to size2x2 or size1x2 as needed
-
 // Add click event listeners to room highlights
 const roomHighlights = document.querySelectorAll('.roomHighlight');
 roomHighlights.forEach(highlight => {
@@ -24,20 +20,8 @@ roomHighlights.forEach(highlight => {
             const roomSizeClass = `size${clickedRoomWidth}x${clickedRoomHeight}`;
             highlight.classList.add('highlighted', roomSizeClass);
 
-            // Update player's position
-            updateHighlightPosition(clickedRoomWidth, clickedRoomHeight, highlight.style.top, highlight.style.left);
-
             // Update current room
             currentRoomId = clickedRoomId;
         }
     });
 });
-
-// Function to update the player's position
-function updateHighlightPosition(roomWidth, roomHeight, topPosition, leftPosition) {
-    const playerRoom = document.getElementById('playerHighlight');
-    playerRoom.style.top = topPosition;
-    playerRoom.style.left = leftPosition;
-    playerRoom.style.width = `${roomWidth * 100}px`;
-    playerRoom.style.height = `${roomHeight * 100}px`;
-}
