3653e8aef9114adfb54ad72a8153ba6343cec7d4
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-25 21:32:08 +0000

    ship test all rooms 15

--- scripts/game.js
+++ scripts/game.js
@@ -11,60 +11,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Define doors between rooms
     const doors = [
-        { roomId: "piloting", targetRoomId: "kitchen", sharedWall: "bottom" },
-        { roomId: "kitchen", targetRoomId: "scanners", sharedWall: "bottom" },
-        { roomId: "kitchen", targetRoomId: "doors", sharedWall: "bottom" },
-        { roomId: "scanners", targetRoomId: "clone_bay", sharedWall: "bottom" },
-        { roomId: "doors", targetRoomId: "shields", sharedWall: "bottom" },
-        { roomId: "clone_bay", targetRoomId: "shields", sharedWall: "right" },
-        { roomId: "clone_bay", targetRoomId: "fabrication", sharedWall: "bottom" },
-        { roomId: "shields", targetRoomId: "weapons", sharedWall: "bottom" },
-        { roomId: "fabrication", targetRoomId: "escape_pods_L", sharedWall: "left" },
-        { roomId: "fabrication", targetRoomId: "weapons", sharedWall: "right" },
-        { roomId: "fabrication", targetRoomId: "electrics", sharedWall: "bottom" },
-        { roomId: "weapons", targetRoomId: "escape_pods_R", sharedWall: "right" },
-        { roomId: "weapons", targetRoomId: "electrics", sharedWall: "bottom" },
-        { roomId: "electrics", targetRoomId: "oxygen", sharedWall: "left" },
-        { roomId: "electrics", targetRoomId: "water", sharedWall: "right" },
-        { roomId: "water", targetRoomId: "security", sharedWall: "bottom" },
-        { roomId: "oxygen", targetRoomId: "recycling", sharedWall: "bottom" },
-        { roomId: "engine", targetRoomId: "recycling", sharedWall: "left" },
-        { roomId: "engine", targetRoomId: "security", sharedWall: "right" },
-        { roomId: "engine", targetRoomId: "storage", sharedWall: "bottom" },
+        { roomId: "piloting", targetRoomId: "kitchen" },
+        { roomId: "kitchen", targetRoomId: "scanners" },
+        { roomId: "kitchen", targetRoomId: "doors" },
+        { roomId: "scanners", targetRoomId: "clone_bay" },
+        { roomId: "doors", targetRoomId: "shields" },
+        { roomId: "clone_bay", targetRoomId: "shields" },
+        { roomId: "clone_bay", targetRoomId: "fabrication" },
+        { roomId: "shields", targetRoomId: "weapons" },
+        { roomId: "fabrication", targetRoomId: "escape_pods_L" },
+        { roomId: "fabrication", targetRoomId: "weapons" },
+        { roomId: "fabrication", targetRoomId: "electrics" },
+        { roomId: "weapons", targetRoomId: "escape_pods_R" },
+        { roomId: "weapons", targetRoomId: "electrics" },
+        { roomId: "electrics", targetRoomId: "oxygen" },
+        { roomId: "electrics", targetRoomId: "water" },
+        { roomId: "water", targetRoomId: "security" },
+        { roomId: "oxygen", targetRoomId: "recycling" },
+        { roomId: "engine", targetRoomId: "recycling" },
+        { roomId: "engine", targetRoomId: "security" },
+        { roomId: "engine", targetRoomId: "storage" },
     ];
 
-    // Function to check if a door is clicked
-    function isDoorClicked(room, mouseX, mouseY, tolerance = 5) {
-        const roomDoors = doors.filter(door => door.roomId === room.id || door.targetRoomId === room.id);
-
-        for (const door of roomDoors) {
-            let doorX, doorY;
-
-            // Calculate door position based on the shared wall
-            switch (door.sharedWall) {
-                case "bottom":
-                    doorX = room.left + (room.width / 2);
-                    doorY = room.top + room.height;
-                    break;
-                case "right":
-                    doorX = room.left + room.width;
-                    doorY = room.top + (room.height / 2);
-                    break;
-                case "left":
-                    doorX = room.left;
-                    doorY = room.top + (room.height / 2);
-                    break;
-            }
-
-            // Check if the click is within the door area with tolerance
-            if (Math.abs(mouseX - doorX) < tolerance && Math.abs(mouseY - doorY) < tolerance) {
-                return true;
-            }
-        }
-
-        return false;
+    // Function to check if a door exists between two rooms
+    function hasDoor(currentRoom, clickedRoom) {
+        return doors.some(door => (door.roomId === currentRoom.id && door.targetRoomId === clickedRoom.id) ||
+                                   (door.roomId === clickedRoom.id && door.targetRoomId === currentRoom.id));
     }
-
 
     // Add click event listeners to room highlights
     const allRoomHighlights = document.querySelectorAll('.roomHighlight1x2, .roomHighlight2x1, .roomHighlight2x2');
@@ -95,28 +68,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 height: parseInt(highlight.getAttribute('data-room-height')) * 100,
             };
 
-            // Check if a door is clicked in either direction
-            const mouseX = event.clientX;
-            const mouseY = event.clientY;
-
-            const doorFromCurrentToClicked = doors.find(door => door.roomId === currentRoom.id && door.targetRoomId === clickedRoom.id);
-            const doorFromClickedToCurrent = doors.find(door => door.roomId === clickedRoom.id && door.targetRoomId === currentRoom.id);
-
-            if (doorFromCurrentToClicked && isDoorClicked(currentRoom, mouseX, mouseY)) {
+            // Check if a door exists between the current room and the clicked room
+            if (hasDoor(currentRoom, clickedRoom)) {
                 // Transition to the target room
                 currentRoom = clickedRoom;
-            } else if (doorFromClickedToCurrent && isDoorClicked(clickedRoom, mouseX, mouseY)) {
-                // Transition to the target room
-                currentRoom = clickedRoom;
+
+                // Remove highlighting from all rooms
+                allRoomHighlights.forEach(room => {
+                    room.classList.remove('highlighted');
+                });
+
+                // Add highlighting to the clicked room
+                highlight.classList.add('highlighted');
             }
-
-            // Remove highlighting from all rooms
-            allRoomHighlights.forEach(room => {
-                room.classList.remove('highlighted');
-            });
-
-            // Add highlighting to the clicked room
-            highlight.classList.add('highlighted');
         });
     });
 });
