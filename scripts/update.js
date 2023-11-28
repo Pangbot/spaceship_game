f31e3140a15555e7b80470b6a04128b46bc8133d
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-28 22:16:21 +0000

    JS restructure 13

--- scripts/update.js
+++ scripts/update.js
@@ -1,10 +1,62 @@
 // update.js
 // Updates stuff.
 import { updateResourceBars } from './resourceBars.js';
+import { highlightAdjacentRooms } from './roomHighlighting.js';
+import { updateButtonDescriptions } from './buttons.js';
 
 export function updateGame() {
     console.log("Continuous update script loaded!");
 
     // Call the function once the DOM is fully loaded
     updateResourceBars();
+
+    // Add click event listeners to room highlights
+    const allRoomHighlights = document.querySelectorAll('.roomHighlight1x2, .roomHighlight2x1, .roomHighlight2x2');
+
+    // Add highlighting to the current room
+    allRoomHighlights.forEach(highlight => {
+        const room = {
+            id: highlight.getAttribute('data-room-id'),
+            top: parseInt(highlight.style.top),
+            left: parseInt(highlight.style.left),
+            width: parseInt(highlight.getAttribute('data-room-width')) * 100,
+            height: parseInt(highlight.getAttribute('data-room-height')) * 100,
+        };
+
+        if (currentRoom.id === room.id) {
+            highlight.classList.add('highlighted');
+        }
+    });
+
+    // Add click event listeners
+    allRoomHighlights.forEach(highlight => {
+        highlight.addEventListener('click', function (event) {
+            const clickedRoom = {
+                id: highlight.getAttribute('data-room-id'),
+                top: parseInt(highlight.style.top),
+                left: parseInt(highlight.style.left),
+                width: parseInt(highlight.getAttribute('data-room-width')) * 100,
+                height: parseInt(highlight.getAttribute('data-room-height')) * 100,
+            };
+
+            if (hasOpenDoor(currentRoom, clickedRoom)) {
+                // Transition to the target room
+                currentRoom = clickedRoom;
+
+                // Remove highlighting from all rooms
+                allRoomHighlights.forEach(room => {
+                    room.classList.remove('highlighted');
+                });
+
+                // Highlight adjacent rooms
+                highlightAdjacentRooms(currentRoom);
+
+                // Add highlighting to the clicked room
+                highlight.classList.add('highlighted');
+
+                // Update button descriptions based on the new room
+                updateButtonDescriptions(currentRoom.id);
+            }
+        });
+    });
 }
