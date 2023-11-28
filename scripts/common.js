f53906a35fbfac45db4235c2b8a7b34969659e7c
Pangbot <idonthaveaclueatall@hotmail.com>
2023-11-28 23:07:18 +0000

    JS restructure 22

--- scripts/common.js
+++ scripts/common.js
@@ -9,4 +9,28 @@ let currentRoom = {
     height: 200,
 };
 
-export { currentRoom };
+// Define doors between rooms
+const doors = [
+    { roomId: "piloting", targetRoomId: "kitchen", status: "closed" },
+    { roomId: "kitchen", targetRoomId: "scanners", status: "open" },
+    { roomId: "kitchen", targetRoomId: "doors", status: "open" },
+    { roomId: "scanners", targetRoomId: "clone_bay", status: "open" },
+    { roomId: "doors", targetRoomId: "shields", status: "open" },
+    { roomId: "clone_bay", targetRoomId: "shields", status: "open" },
+    { roomId: "clone_bay", targetRoomId: "fabrication", status: "open" },
+    { roomId: "shields", targetRoomId: "weapons", status: "open" },
+    { roomId: "fabrication", targetRoomId: "escape_pods_L", status: "open" },
+    { roomId: "fabrication", targetRoomId: "weapons", status: "open" },
+    { roomId: "fabrication", targetRoomId: "electrics", status: "open" },
+    { roomId: "weapons", targetRoomId: "escape_pods_R", status: "open" },
+    { roomId: "weapons", targetRoomId: "electrics", status: "open" },
+    { roomId: "electrics", targetRoomId: "oxygen", status: "open" },
+    { roomId: "electrics", targetRoomId: "water", status: "open" },
+    { roomId: "water", targetRoomId: "security", status: "open" },
+    { roomId: "oxygen", targetRoomId: "recycling", status: "open" },
+    { roomId: "engine", targetRoomId: "recycling", status: "open" },
+    { roomId: "engine", targetRoomId: "security", status: "open" },
+    { roomId: "engine", targetRoomId: "storage", status: "closed" },
+];
+
+export { currentRoom, doors };
--- scripts/roomHighlighting.js
+++ scripts/roomHighlighting.js
@@ -1,29 +1,7 @@
 // roomHighlighting.js
 // Contains the logic for highlighting rooms on the ship map.
 
-// Define doors between rooms
-const doors = [
-    { roomId: "piloting", targetRoomId: "kitchen", status: "closed" },
-    { roomId: "kitchen", targetRoomId: "scanners", status: "open" },
-    { roomId: "kitchen", targetRoomId: "doors", status: "open" },
-    { roomId: "scanners", targetRoomId: "clone_bay", status: "open" },
-    { roomId: "doors", targetRoomId: "shields", status: "open" },
-    { roomId: "clone_bay", targetRoomId: "shields", status: "open" },
-    { roomId: "clone_bay", targetRoomId: "fabrication", status: "open" },
-    { roomId: "shields", targetRoomId: "weapons", status: "open" },
-    { roomId: "fabrication", targetRoomId: "escape_pods_L", status: "open" },
-    { roomId: "fabrication", targetRoomId: "weapons", status: "open" },
-    { roomId: "fabrication", targetRoomId: "electrics", status: "open" },
-    { roomId: "weapons", targetRoomId: "escape_pods_R", status: "open" },
-    { roomId: "weapons", targetRoomId: "electrics", status: "open" },
-    { roomId: "electrics", targetRoomId: "oxygen", status: "open" },
-    { roomId: "electrics", targetRoomId: "water", status: "open" },
-    { roomId: "water", targetRoomId: "security", status: "open" },
-    { roomId: "oxygen", targetRoomId: "recycling", status: "open" },
-    { roomId: "engine", targetRoomId: "recycling", status: "open" },
-    { roomId: "engine", targetRoomId: "security", status: "open" },
-    { roomId: "engine", targetRoomId: "storage", status: "closed" },
-];
+import { doors } from './common.js';
 
 // Function to check if a door exists between two rooms
 function hasOpenDoor(currentRoom, clickedRoom) {
