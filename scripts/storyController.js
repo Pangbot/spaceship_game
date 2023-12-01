2046166da7b0f856decf67da8d327b022b637135
Pangbot <idonthaveaclueatall@hotmail.com>
2023-12-01 01:25:59 +0000

    Story as popups 12

--- scripts/storyController.js
+++ scripts/storyController.js
@@ -1,5 +1,5 @@
 // storyController.js
-import { isUpdateEnabled, setStoryStatus, setUpdateStatus, storyTime } from './common.js';
+import { setStoryStatus, setUpdateStatus } from './common.js';
 
 const messages = [
   "Welcome to the game!",
@@ -10,36 +10,31 @@ const messages = [
 ];
 
 let currentMessageIndex = 0;
-let nextButtonCreated = false; // Flag to track if the button is created
 
 function showPopup() {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
     displayMessage();
+    createNextButton(); // Create "Next" button dynamically
 }
 
 function displayMessage() {
     document.getElementById('message').innerText = messages[currentMessageIndex];
 }
 
+function createNextButton() {
+    // Create "Next" button dynamically
+    const nextButton = document.createElement('button');
+    nextButton.innerText = 'Next';
+    nextButton.onclick = nextMessage; // Attach the onclick event handler
+
+    // Append the button to the popup container
+    document.getElementById('popup').appendChild(nextButton);
+}
+
 function runStoryEvent() {
     if (currentMessageIndex < messages.length) {
         showPopup();
-        displayMessage();
-
-        // Check if the button is not created
-        if (!nextButtonCreated) {
-            // Create "Next" button dynamically
-            const nextButton = document.createElement('button');
-            nextButton.innerText = 'Next';
-            nextButton.onclick = nextMessage; // Attach the onclick event handler
-
-            // Append the button to the popup container
-            document.getElementById('popup').appendChild(nextButton);
-
-            // Set the flag to true, indicating that the button is created
-            nextButtonCreated = true;
-        }
     } else {
         document.getElementById('overlay').style.display = 'none';
         document.getElementById('popup').style.display = 'none';
@@ -58,9 +53,6 @@ function runStoryEvent() {
 
         setUpdateStatus(true);
         setStoryStatus(false);
-
-        // Reset the flag for the next story event
-        nextButtonCreated = false;
     }
 }
 
