96385232294541ee920d1b081c5d10c24ba14249
Pangbot <idonthaveaclueatall@hotmail.com>
2023-12-02 01:42:30 +0000

    Story as popups 33

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@
 import { initialiseGame } from './init.js';
 import { updateGame } from './update.js';
 import { isUpdateEnabled, storyTime } from './common.js';
-import { runStoryEvent, handleCloseButtonClick, handleNextButtonClick } from './storyController.js';
+import { runStoryEvent } from './storyController.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log("Game script loaded!");
@@ -10,18 +10,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialise the game
     initialiseGame();
 
-    let waitForButtonClick = false;
-
     function gameLoop() {
         if (isUpdateEnabled) {
             // Call the continuous update function
             updateGame();
         } else if (storyTime) {
             runStoryEvent();
-            if (waitForButtonClick) {
-                waitForButtonClick = false;
-                return;
-            }
         } else {
             console.error("I DON'T KNOW WHAT TO DO");
         }
@@ -32,10 +26,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Start the game loop
     gameLoop();
-
-    // Add an event listener for the "Next" button click
-    document.getElementById('popup').addEventListener('click', function () {
-        waitForButtonClick = true;
-        runStoryEvent();
-    });
 });
