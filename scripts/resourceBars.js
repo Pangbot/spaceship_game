7ee9b9c9100adec375d72e516596a93466cac6cf
Pangbot <idonthaveaclueatall@hotmail.com>
2023-12-04 02:25:50 +0000

    Story as popups 51

--- scripts/resourceBars.js
+++ scripts/resourceBars.js
@@ -26,44 +26,44 @@ function updateResourceBars() {
     });
 
     function updateBars() {
-        const now = performance.now();
-        const elapsedMilliseconds = now - (lastTimestamp || now);
-    
-        if (!lastTimestamp) {
-            lastTimestamp = now;
-        }
-    
-        if (!storyTime) {
-            bars.forEach((bar) => {
-                const barElement = document.getElementById(bar.id);
-                const percentageElement = barElement.closest('.resource-bar-container').querySelector('.percentage');
-    
-                if (bar.id === 'food_bar' && Math.round(bar.currentBar) === 93 && storyMessages[0].message_shown === false) {
-                    console.log('story time! (mandatory)');
-                    setStoryStatus(true);
-                }
-    
-                const decreaseRate = bar.rate * (elapsedMilliseconds / 1000);
-                bar.currentBar = Math.max(0, bar.currentBar - decreaseRate);
-    
-                const currentWidth = bar.currentBar;
-                barElement.style.width = `${currentWidth}%`;
-                barElement.setAttribute('data-fill', bar.currentBar);
-                percentageElement.innerText = `${Math.round(bar.currentBar)}%`;
-            });
-        }
-    
-        // Check if a story event needs to be called
-        if (storyTime) {
-            setUpdateStatus(false);
-            cancelAnimationFrame(animationFrameId);
-        } else {
-            // Use requestAnimationFrame for the next update
-            animationFrameId = requestAnimationFrame(updateBars);
-            lastTimestamp = now;
-        }
+    const now = performance.now();
+    const elapsedMilliseconds = now - (lastTimestamp || now);
+
+    if (!lastTimestamp) {
+        lastTimestamp = now;
     }
-    
+
+    if (!storyTime) {
+        bars.forEach((bar) => {
+            const barElement = document.getElementById(bar.id);
+            const percentageElement = barElement.closest('.resource-bar-container').querySelector('.percentage');
+
+            if (bar.id === 'food_bar' && Math.round(bar.currentBar) === 93 && storyMessages[0].message_shown === false) {
+                console.log('story time! (mandatory)');
+                setStoryStatus(true);
+            }
+
+            const decreaseRate = bar.rate * (elapsedMilliseconds / 1000);
+            bar.currentBar = Math.max(0, bar.currentBar - decreaseRate);
+
+            const currentWidth = bar.currentBar;
+            barElement.style.width = `${currentWidth}%`;
+            barElement.setAttribute('data-fill', bar.currentBar);
+            percentageElement.innerText = `${Math.round(bar.currentBar)}%`;
+        });
+    }
+
+    // Check if a story event needs to be called
+    if (storyTime) {
+        setUpdateStatus(false);
+        cancelAnimationFrame(animationFrameId);
+    } else {
+        // Use requestAnimationFrame for the next update
+        animationFrameId = requestAnimationFrame(updateBars);
+        lastTimestamp = now;
+    }
+}
+
 
     // Initial call to start the recursive process
     animationFrameId = requestAnimationFrame(updateBars);
