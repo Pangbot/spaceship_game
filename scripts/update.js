1ec3d3af8feb64986b77f0536bd0e09019e33bde
Pangbot <idonthaveaclueatall@hotmail.com>
2023-12-10 00:41:50 +0000

    Memory fixing?

--- scripts/update.js
+++ scripts/update.js
@@ -6,26 +6,28 @@ import { updateButtonDescriptions } from './buttons.js';
 import { currentRoom, doors, lastMessageClicked, setLastMessageClicked, isGamePaused } from './common.js';
 
 export function updateGame() {
-    
-    if(lastMessageClicked) {
+    if (lastMessageClicked) {
         updateResourceBars();
         setLastMessageClicked(false);
     }
 
-    // Add click event listeners to room highlights
-    const allRoomHighlights = document.querySelectorAll('.roomHighlight1x2, .roomHighlight2x1, .roomHighlight2x2');
+    // Delegate click event to a common ancestor
+    const commonAncestor = document.querySelector('.container-of-all-room-highlights');
 
-    allRoomHighlights.forEach(highlight => {
-        highlight.addEventListener('click', function (event) {
-            const clickedRoomId = highlight.getAttribute('data-room-id');
+    commonAncestor.addEventListener('click', function (event) {
+        const target = event.target;
+
+        // Check if the clicked element has the class 'roomHighlight1x2', 'roomHighlight2x1', or 'roomHighlight2x2'
+        if (target.matches('.roomHighlight1x2, .roomHighlight2x1, .roomHighlight2x2')) {
+            const clickedRoomId = target.getAttribute('data-room-id');
 
             if (hasOpenDoor(currentRoom.id, clickedRoomId, doors)) {
                 // Transition to the target room
                 currentRoom.id = clickedRoomId;
-                currentRoom.top = parseInt(highlight.style.top);
-                currentRoom.left = parseInt(highlight.style.left);
-                currentRoom.width = parseInt(highlight.getAttribute('data-room-width')) * 50;
-                currentRoom.height = parseInt(highlight.getAttribute('data-room-height')) * 50;
+                currentRoom.top = parseInt(target.style.top);
+                currentRoom.left = parseInt(target.style.left);
+                currentRoom.width = parseInt(target.getAttribute('data-room-width')) * 50;
+                currentRoom.height = parseInt(target.getAttribute('data-room-height')) * 50;
 
                 // Update room highlights
                 highlightAdjacentRooms(currentRoom.id);
@@ -33,6 +35,6 @@ export function updateGame() {
                 // Update button descriptions based on the new room
                 updateButtonDescriptions(currentRoom.id);
             }
-        });
+        }
     });
 }
